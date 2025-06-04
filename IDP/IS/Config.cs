06b319f8ea4a11332012527d18c794af36d19785
Abbas <mr_yavar@outlook.com>
2025-06-05 00:46:02 +0330

    'init/33'

---
+++ IDP/IS/Config.cs
@@ -0,0 +1,24 @@
+﻿using Duende.IdentityServer.Models;
+
+namespace IS
+{
+    public static class Config
+    {
+        public static IEnumerable<IdentityResource> IdentityResources =>
+            new IdentityResource[]
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile()
+            };
+
+        public static IEnumerable<ApiScope> ApiScopes =>
+            new ApiScope[]
+                { };
+        public static IEnumerable<ApiResource> Apis =>
+new ApiResource[]
+{ };
+        public static IEnumerable<Client> Clients =>
+            new Client[]
+                { };
+    }
+}
